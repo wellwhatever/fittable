a85392f2fd90bde9fb8f03f969f154bdfd56d51e
oleksandrpetrov <oleksandr.petrov@adastra.one>
2023-10-23 22:48:14 +0200

    Create data store

---
+++ shared/src/commonMain/kotlin/cz/cvut/fit/fittable/shared/data/DataStore.kt
@@ -0,0 +1,14 @@
+package cz.cvut.fit.fittable.shared.data
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import okio.Path.Companion.toPath
+
+fun createDataStore(
+    producePath: () -> String,
+): DataStore<Preferences> = PreferenceDataStoreFactory.createWithPath(
+    corruptionHandler = null,
+    migrations = emptyList(),
+    produceFile = { producePath().toPath() },
+)
