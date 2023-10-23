55973c9232724f3380c5ecb573dd956b9cc489f5
oleksandrpetrov <oleksandr.petrov@adastra.one>
2023-10-23 22:47:27 +0200

    Add IOS project to repository

---
+++ iosApp/fittableIOS/fittableIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  fittableIOSApp.swift
+//  fittableIOS
+//
+//  Created by Oleksandr Petrov on 21.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct fittableIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
